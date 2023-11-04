3602540f80cecbd15a44cec64f06df1366a55a6a
Lawfty Goals <mymsnadressis@hotmail.com>
2023-11-04 18:15:31 +0100

    update

--- src/script.js
+++ src/script.js
@@ -5,12 +5,19 @@ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls
 import { createArmMesh } from "./armGroup.js"
 
 
+const XZPLANE_SIDELENGTH = 100;
 
 //console.log(window.location.pathname);
 
 const ri = {
     currentlyPressedKeys: []
 }
+
+const phy = {
+    rigidBodies: [],
+    transform: undefined
+}
+
 // Her legger man til three.js koden.
 export function main() {
 
@@ -21,6 +28,7 @@ export function main() {
     let target = new THREE.Vector3(0.0, 0.0, 0.0);
     ri.camera.lookAt(target);
 
+    createAmmoWorld();
     /**
      * Trackball control implementation
      */
@@ -39,7 +47,21 @@ export function main() {
     document.addEventListener('keydown', handleKeyDown, false)
 
 
-    addSceneObjects();
+    addAmmoSceneObjects();
+
+}
+
+function createAmmoWorld() {
+    phy.transform = new Ammo.btTransform();
+
+    let collisionConfiguration = new Ammo.btDefaultCollisionConfiguration(),
+        dispatcher = new Ammo.btCollisionDispatcher(collisionConfiguration),
+        overlappingPairCache = new Ammo.btDbvtBroadphase(),
+        solver = new Ammo.btSequentialImpulseConstraintSolver();
+
+
+    phy.ammoPhysicsWorld = new Ammo.btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    phy.ammoPhysicsWorld.setGravity(new Ammo.btVector3(0, -9.81, 0))
 
 }
 
@@ -112,6 +134,41 @@ function addLights() {
 
     ri.scene.add(lightCamHelper);
     ri.scene.add(directionalLight);
+
+
+}
+
+function addAmmoSceneObjects() {
+    createAmmoXZPlane();
+
+    //animate(0);
+}
+
+function createAmmoXZPlane() {
+
+    let material = new THREE.MeshStandardMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+
+    let geometry = new THREE.PlaneGeometry(XZPLANE_SIDELENGTH, XZPLANE_SIDELENGTH, 1, 1);
+    geometry.rotateX(-Math.PI / 2);
+
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.receiveShadow = true;
+    mesh.name = 'xzplane';
+
+    const mass = 0;
+    const position = { x: 0, y: 0, z: 0 };
+
+    let shape = new Ammo.btBoxShape(new Ammo.btVector3(XZPLANE_SIDELENGTH / 2, 0, XZPLANE_SIDELENGTH / 2));
+    let rigidBody = createAmmoRigidBody(shape, mesh, 0.7, 0.8, position, mass);
+
+    mesh.userData.phsyicsBody = rigidBody;
+
+    phy.ammoPhysicsWorld.addRigidBody(rigidBody);
+
+    ri.scene.add(mesh);
+    phy.rigidBodies.push(mesh);
+    rigidBody.threeMesh = mesh;
+
 
 
 }
@@ -215,4 +272,33 @@ function handleKeys(delta, arm) {
         arm.joint2Rot = arm.joint2Rot - (rotationSpeed * delta);
         arm.joint2Rot %= (Math.PI * 2);
     }
+}
+
+
+function createAmmoRigidBody(shape, threeMesh, restitution = 0.7, friction = 0.8, position = { x: 0, y: 50, z: 0 }, mass = 1, useLocalScaling = true) {
+    let transform = new Ammo.btTransform();
+
+    transform.setIdentity();
+    transform.setOrigin(new Ammo.btVector3(position.x, position.y, position.z));
+
+    let quaternion = threeMesh.quaternion;
+    transform.setRotation(new Ammo.btQuaternion(quaternion.x, quaternion.y, quaternion.z, quaternion.w));
+
+    if (useLocalScaling) {
+        let scale = threeMesh.scale;
+        shape.setLocalScaling(new Ammo.btVector3(scale.x, scale.y, scale.z));
+    }
+
+    let motionState = new Ammo.btDefaultMotionState(transform);
+    let localInertia = new Ammo.btVector3(0, 0, 0);
+    shape.calculateLocalInertia(mass, localInertia);
+
+    let rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, motionState, shape, localInertia);
+
+    let rigidBody = new Ammo.btRigidBody(rbInfo);
+    rigidBody.setRestitution(restitution);
+    rigidBody.setFriction(friction);
+
+    return rigidBody;
+
 }
